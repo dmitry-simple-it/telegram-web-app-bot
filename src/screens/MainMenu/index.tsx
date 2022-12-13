4f1b121f54bf538187e4098a093f29f0c1a331ac
Victor Vysotskiy <vikont7@yahoo.com>
2022-12-13 17:15:23 +0300

    Fixed main screen: - Size of the icons and elements' placement - Web app now expands on opening the app

--- src/screens/MainMenu/index.tsx
+++ src/screens/MainMenu/index.tsx
@@ -1,13 +1,18 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { menuItems } from './const';
 import MenuItem from './MenuItem';
+import { tgWebApp } from '../../components/Telegram';
 
 import './style.scss';
 
 const MainMenu: FC = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    tgWebApp.expand();
+  }, []);
 
   return (
     <div className="screen main-menu">
